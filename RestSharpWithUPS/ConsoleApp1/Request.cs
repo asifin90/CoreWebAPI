f76bb9c1539d668a9a48d3b513fb7173e59d1f62
Asif Sayyad <asifin90@gmail.com>
2021-09-09 09:55:33 +0530

    Update Request.cs
    Updated mode with deliveryTimeInformation.

--- RestSharpWithUPS/ConsoleApp1/Request.cs
+++ RestSharpWithUPS/ConsoleApp1/Request.cs
@@ -1,23 +1,23 @@
-﻿
+
 
 namespace ConsoleApp1
 {
     public class TransactionReference
     {
         public string CustomerContext { get; set; }
-    }
 
+    }
     public class Request
     {
         public string SubVersion { get; set; }
         public TransactionReference TransactionReference { get; set; }
-    }
 
+    }
     public class ShipmentRatingOptions
     {
         public string UserLevelDiscountIndicator { get; set; }
-    }
 
+    }
     public class Address
     {
         public string AddressLine { get; set; }
@@ -25,49 +25,52 @@ namespace ConsoleApp1
         public string StateProvinceCode { get; set; }
         public string PostalCode { get; set; }
         public string CountryCode { get; set; }
-    }
 
+    }
     public class Shipper
     {
         public string Name { get; set; }
         public string ShipperNumber { get; set; }
         public Address Address { get; set; }
-    }
 
+    }
+ 
     public class ShipTo
     {
         public string Name { get; set; }
         public Address Address { get; set; }
+
     }
 
     public class ShipFrom
     {
         public string Name { get; set; }
         public Address Address { get; set; }
-    }
 
+    }
     public class Service
     {
         public string Code { get; set; }
         public string Description { get; set; }
-    }
 
+    }
     public class UnitOfMeasurement
     {
         public string Code { get; set; }
         public string Description { get; set; }
-    }
 
+    }
     public class ShipmentTotalWeight
     {
         public UnitOfMeasurement UnitOfMeasurement { get; set; }
         public string Weight { get; set; }
-    }
 
+    }
     public class PackagingType
     {
         public string Code { get; set; }
         public string Description { get; set; }
+
     }
 
     public class Dimensions
@@ -76,21 +79,39 @@ namespace ConsoleApp1
         public string Length { get; set; }
         public string Width { get; set; }
         public string Height { get; set; }
+
     }
 
     public class PackageWeight
     {
         public UnitOfMeasurement UnitOfMeasurement { get; set; }
         public string Weight { get; set; }
-    }
 
+    }
     public class Package
     {
         public PackagingType PackagingType { get; set; }
         public Dimensions Dimensions { get; set; }
         public PackageWeight PackageWeight { get; set; }
-    }
 
+    }
+    public class ShipmentServiceOptions
+    {
+        public string SaturdayPickupIndicator { get; set; }
+
+    }
+    public class Pickup
+    {
+        public string Date { get; set; }
+        public string Time { get; set; }
+
+    }
+    public class DeliveryTimeInformation
+    {
+        public string PackageBillType { get; set; }
+        public Pickup Pickup { get; set; }
+
+    }
     public class Shipment
     {
         public ShipmentRatingOptions ShipmentRatingOptions { get; set; }
@@ -100,18 +121,20 @@ namespace ConsoleApp1
         public Service Service { get; set; }
         public ShipmentTotalWeight ShipmentTotalWeight { get; set; }
         public Package Package { get; set; }
-    }
+        public ShipmentServiceOptions ShipmentServiceOptions { get; set; }
+        public DeliveryTimeInformation DeliveryTimeInformation { get; set; }
 
+    }
     public class RateRequest
     {
         public Request Request { get; set; }
         public Shipment Shipment { get; set; }
-    }
 
-    public class Root
+    }
+    public class Application
     {
         public RateRequest RateRequest { get; set; }
-    }
 
+    }
 
 }
